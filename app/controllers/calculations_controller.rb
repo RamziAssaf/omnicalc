fb002cbafef594c25b3c48bd388b125da438cd77
RamziAssaf <rassaf@chicagobooth.edu>
2015-10-13 21:55:18 -0500

    HW

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,13 +11,13 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.gsub(" ","").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.length
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.scan(@special_word).count
 
     # ================================================================================
     # Your code goes above.
@@ -28,7 +28,7 @@ class CalculationsController < ApplicationController
 
   def loan_payment
     @apr = params[:annual_percentage_rate].to_f
-    @years = params[:number_of_years].to_i
+    @years = params[:numberf_years].to_i
     @principal = params[:principal_value].to_f
 
     # ================================================================================
@@ -38,7 +38,7 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = @principal * ( (1+@apr/100)**@years )
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +60,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = (@ending-@starting)
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @weeks/52
 
     # ================================================================================
     # Your code goes above.
@@ -82,27 +82,50 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum =@numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    if @count.odd?
+        @median = @sorted_numbers[@count/2]
+    else @count.even?
+        @median = ( @sorted_numbers[(@count/2)-1] + @sorted_numbers[@count/2] ) / 2
+    end
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @variance = "Replace this string with your answer."
+    var_sum = 0
+    @numbers.each do |x|
+        var_sum = var_sum + (x-@mean)**2
+    end
+    @variance = 1/(@count-1) * var_sum
 
-    @standard_deviation = "Replace this string with your answer."
 
-    @mode = "Replace this string with your answer."
+    @standard_deviation = Math.sqrt(@variance)
+
+    # MODE
+    num_occurring = []
+    x=0
+    while x< @count
+        num_occurring[x] = 0
+        y=0
+        while y<@count
+            if @numbers[x]==@numbers[y]
+                num_occurring[x]= num_occurring[x] + 1
+            end
+            y=y+1
+        end
+        x=x+1
+    end
+    @mode = @numbers [ num_occurring.index(num_occurring.max) ]
 
     # ================================================================================
     # Your code goes above.
